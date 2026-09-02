52ca0e9f6e808175c116ce73d8b502d7fb0282f2
12asj <12asjuancamilo@gmail.com>
2026-09-02 17:12:46 -0500

    cambios

--- src/main/java/com/example/Veterinaria/Emtity/Propietario.java
+++ src/main/java/com/example/Veterinaria/Emtity/Propietario.java
@@ -1,4 +1,45 @@
 package com.example.Veterinaria.Emtity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Propietario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es Obligatorio")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "El Documento es obligatorio")
+    @Column(nullable = false, unique = true)
+    private String documento;
+
+    @NotBlank(message = "El Telefono es Obligatorio")
+    @Column(nullable = false)
+    private  String telefono;
+
+    @NotBlank(message = "El nombre es Obligatorio")
+    @Email(message = "El correo electronico debe ser valido ")
+    @Column(nullable = false, unique = false)
+    private  String correo;
+
+    @OneToMany(mappedBy = "propietario", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("propietariio")
+    private List<Mascota> mascotas = new ArrayList<>();
 }
--- src/main/java/com/example/Veterinaria/Emtity/Veterinario.java
+++ src/main/java/com/example/Veterinaria/Emtity/Veterinario.java
@@ -1,5 +1,45 @@
 package com.example.Veterinaria.Emtity;
 
-public class Veterinario {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Veterinario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es Obligatorio")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "La tarjeta profesional es obligatoria")
+    @Column(nullable = false, unique = true)
+    private String tarjeta_profecional;
+
+    @NotBlank(message = "este campo no puede estar vacio")
+    @Column(nullable = false)
+    private String especialidad;
+
+    @NotBlank(message = "El nombre es Obligatorio")
+    @Email(message = "El correo electronico debe ser valido ")
+    @Column(nullable = false, unique = false)
+    private  String correo;
+
+    @ManyToMany(mappedBy = "veterinarios")
+    @JsonIgnoreProperties("veterinarios")
+    private List<Mascota> mascotas = new ArrayList<>();
 }
